35ae32ed75755ae6c7b6a0c2b51faefc7e83f568
Radmiurr <124788354+Radmiurr@users.noreply.github.com>
2024-03-26 08:35:18 +0600

    Update Class1.java

--- Lab1/Lab1/src/Class1.java
+++ Lab1/Lab1/src/Class1.java
@@ -3,23 +3,23 @@ import java.util.Scanner;
 public class Class1 implements IProcessText {
     public String processText() {
         Scanner scanner = new Scanner(System.in);
-        String inputText = scanner.nextLine();
-        return inputText;
+        String _inputText = scanner.nextLine();
+        return _inputText;
     }
 
     // Операция 1
-    protected void operation1(String text) {
+    protected void operation1(String _text) {
 
-        String[] sentences = text.split("\\. ");
+        String[] sentences = _text.split("\\. ");
         int _count = 0;
 
         for (String sentence : sentences) {
-            var words = sentence.split(" ");
-            int length = words.length;
+            var _words = sentence.split(" ");
+            int _length = _words.length;
 
-            if (length > 3) {
+            if (_length > 3) {
                 System.out.println(sentence);
-                System.out.println("Длина предложения равна:" + length);
+                System.out.println("Длина предложения равна:" + _length);
                 _count++;
             }
         }
