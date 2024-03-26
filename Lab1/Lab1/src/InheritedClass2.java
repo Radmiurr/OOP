35926440b1f3c91b3fd6a224a5f3499949880983
Radmiurr <124788354+Radmiurr@users.noreply.github.com>
2024-03-26 08:58:33 +0600

    Update InheritedClass2.java

--- Lab1/Lab1/src/InheritedClass2.java
+++ Lab1/Lab1/src/InheritedClass2.java
@@ -1,11 +1,11 @@
 public class InheritedClass2 extends InheritedClass1 {
     @Override
     protected void operation1(String _text) {
-        var sentences = _text.split("\\. ");
+        var _sentences = _text.split("\\. ");
         int _count = 0;
-        for(String sentence : sentences){
-            if (sentence.split(" ").length < 5){
-                System.out.println(sentence);
+        for(String _sentence : _sentences){
+            if (_sentence.split(" ").length < 5){
+                System.out.println(_sentence);
                 _count++;
             }
         }
